c38a650ae06b45c435ed3aa167c3f9f16af3c765
tuyenlt <keplerlu@gmail.com>
2025-05-29 20:06:11 +0700

    update

--- src/pages/classes/ClassAssignmentSummary.jsx
+++ src/pages/classes/ClassAssignmentSummary.jsx
@@ -1,12 +1,36 @@
+import { useUserContext } from '@/providers/authContext';
 import ClassExerciseCard from './ClassExerciseCard';
+import { useEffect, useState } from 'react';
+import api from '@/services/api';
 
 export default function ClassAssignmentSummary() {
-  return (
-    <div className="grid grid-flow-row col-start-4 row-span-full">
-      <h2 className="text-neutral-700 text-2xl">My Assignment</h2>
-      <ClassExerciseCard></ClassExerciseCard>
-      <ClassExerciseCard></ClassExerciseCard>
-      <ClassExerciseCard></ClassExerciseCard>
-    </div>
-  );
+	const { user } = useUserContext();
+	const [assignments, setAssignments] = useState([]);
+
+	// Fetch assignments based on user role
+	useEffect(() => {
+		api.get(`/assignments/user/${user?._id}/`)
+			.then((res) => {
+				const data = res.data;
+				const filteredAssignments = data.filter(assignment => assignment.due_date && new Date(assignment.due_date) >= new Date());
+				setAssignments(filteredAssignments);
+			}).catch((err) => {
+				console.error('Error fetching assignments:', err);
+			});
+	}, [user]);
+
+	return (
+		<div className="flex flex-col items-center row-span-full">
+			<h2 className="text-neutral-700 text-2xl">My Assignment</h2>
+			{assignments.length === 0 ? (
+				<div className="flex items-center justify-center h-full text-gray-500">
+					<p className="text-sm">No assignments on due</p>
+				</div>
+			) : (
+				assignments.map((assignment, idx) => (
+					<ClassExerciseCard key={idx} assignment={assignment} />
+				))
+			)}
+		</div>
+	);
 }
--- src/pages/classes/ClassPage.jsx
+++ src/pages/classes/ClassPage.jsx
@@ -6,62 +6,66 @@ import { useEffect, useState } from 'react';
 import api from '@/services/api';
 import LoadingSpinner from '@/components/shared/LoadingSpinner';
 export default function ClassPage() {
-  const { user } = useUserContext();
-  const isStudent = user?.role === 'student';
-  const isTeacher = user?.role === 'teacher';
-  const [classData, setClassData] = useState([]);
-  const [fallbackColor, setFallbackColor] = useState();
-  const fallbackColors = ['red', 'orange', 'purple', 'green', 'gray', 'blue'];
-  const [loading, setLoading] = useState(false);
+	const { user } = useUserContext();
+	const isStudent = user?.role === 'student';
+	const isTeacher = user?.role === 'teacher';
+	const [classData, setClassData] = useState([]);
+	const [fallbackColor, setFallbackColor] = useState();
+	const fallbackColors = ['red', 'orange', 'purple', 'green', 'gray', 'blue'];
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setFallbackColor(fallbackColors[Math.floor(Math.random() * 6)]);
-  }, []);
+	useEffect(() => {
+		setFallbackColor(fallbackColors[Math.floor(Math.random() * 6)]);
+	}, []);
 
-  useEffect(() => {
-    const fetchClassData = async () => {
-      setLoading(true);
-      try {
-        const response = await api.get(`/classes-of-user/${user._id}`);
-        setClassData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching class data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchClassData();
-  }, []);
-  if (loading) {
-    return <div className='m-auto'>
-      <LoadingSpinner />
-    </div>;
-  }
+	useEffect(() => {
+		const fetchClassData = async () => {
+			setLoading(true);
+			try {
+				const response = await api.get(`/classes-of-user/${user._id}`, {
+					params: {
+						populate_fields: ['class_sessions'],
+					},
+				});
+				setClassData(response.data);
+				console.log(response.data);
+			} catch (error) {
+				console.error('Error fetching class data:', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchClassData();
+	}, []);
+	if (loading) {
+		return <div className='m-auto'>
+			<LoadingSpinner />
+		</div>;
+	}
 
-  return (
-    <div className="grid grid-cols-4 items-start gap-y-7 p-6 max-w-screen-xl m-auto">
-      {classData?.length === 0 &&
-        <div className="flex items-center justify-center h-screen">
-          <p className="text-gray-500">No class available</p>
-        </div>
-      }
-      <div
-        className={cn({
-          'col-span-3 grid-cols-3 grid gap-y-12': isStudent,
-          'col-span-4 grid-cols-4 grid gap-y-12': isTeacher,
-        })}
-      >
-        {classData?.map((data, idx) => (
-          <ClassCard key={idx} data={data} fallbackColor={fallbackColor} />
-        ))}
-      </div>
+	return (
+		<div className="grid grid-cols-4 items-start gap-y-7 p-6 max-w-screen-2xl m-auto">
+			{classData?.length === 0 &&
+				<div className="flex items-center justify-center h-screen">
+					<p className="text-gray-500">No class available</p>
+				</div>
+			}
+			<div
+				className={cn("mt-4", {
+					'col-span-3 grid-cols-3 grid gap-y-12': isStudent,
+					'col-span-4 grid-cols-4 grid gap-y-12': isTeacher,
+				})}
+			>
+				{classData?.map((data, idx) => (
+					<ClassCard key={idx} data={data} fallbackColor={fallbackColor} />
+				))}
+			</div>
 
-      {isStudent && (
-        <div className="col-span-1">
-          <ClassAssignmentSummary />
-        </div>
-      )}
-    </div>
-  );
+			{isStudent && (
+				<div className="col-span-1">
+					<ClassAssignmentSummary />
+				</div>
+			)}
+		</div>
+	);
 }
