b31889e37c7ae23452a00e3d0cd6f9d3014395dc
tuyenlt <keplerlu@gmail.com>
2025-05-31 19:46:19 +0700

    push build

--- src/pages/classes/components/Comment.jsx
+++ src/pages/classes/components/Comment.jsx
@@ -5,38 +5,37 @@ import { useState } from 'react';
 import MagicInput from '@/components/shared/MagicInput';
 
 export default function Comment({ postId, onSubmit }) {
-  const [comment, setComment] = useState('');
+	const [comment, setComment] = useState('');
 
-  const isCommentEmpty = comment.trim() === '';
+	const isCommentEmpty = comment.trim() === '';
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!isCommentEmpty) {
-      onSubmit(postId, comment);
-      console.log(comment);
-      setComment('');
-    }
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!isCommentEmpty) {
+			onSubmit(postId, comment);
+			setComment('');
+		}
+	};
 
-  return (
-    <form className="flex w-full gap-x-3" onSubmit={handleSubmit}>
-      <MagicInput
-        placeholder="Write a comment..."
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-      />
-      <button
-        type="submit"
-        disabled={isCommentEmpty}
-        className={cn(
-          'transition-colors',
-          isCommentEmpty
-            ? 'text-gray-300 cursor-not-allowed'
-            : 'text-primary hover:text-primary/80'
-        )}
-      >
-        <SendHorizonal />
-      </button>
-    </form>
-  );
+	return (
+		<form className="flex w-full gap-x-3" onSubmit={handleSubmit}>
+			<MagicInput
+				placeholder="Write a comment..."
+				value={comment}
+				onChange={(e) => setComment(e.target.value)}
+			/>
+			<button
+				type="submit"
+				disabled={isCommentEmpty}
+				className={cn(
+					'transition-colors',
+					isCommentEmpty
+						? 'text-gray-300 cursor-not-allowed'
+						: 'text-primary hover:text-primary/80'
+				)}
+			>
+				<SendHorizonal />
+			</button>
+		</form>
+	);
 }
