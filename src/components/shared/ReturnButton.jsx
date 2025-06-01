2e175fc2cad59958707fa6d659400e02424e447f
tuyenlt <keplerlu@gmail.com>
2025-06-01 18:57:09 +0700

    udpate domain

--- src/components/shared/ReturnButton.jsx
+++ src/components/shared/ReturnButton.jsx
@@ -1,10 +1,15 @@
-import { ArrowLeft } from "lucide-react";
+import { useState } from 'react';
 
-export default function ReturnButton({ className, ...props }) {
-    return (
-        <div className={`flex text-lg text-muted-foreground items-center gap-2 cursor-pointer font-bold ${className ? className : ""}`} {...props}>
-            <ArrowLeft className="w-6 h-6 border-2 border-muted-foreground rounded-md" />
-            <span>Back</span>
-        </div>
-    );
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div style={{ textAlign: 'center', marginTop: '100px' }}>
+			<h1>Đếm số: {count}</h1>
+			<button onClick={() => setCount(count - 1)}>-</button>
+			<button onClick={() => setCount(count + 1)}>+</button>
+		</div>
+	);
 }
+
+export default App;
